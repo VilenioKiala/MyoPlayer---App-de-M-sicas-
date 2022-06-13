de92ec1822e8d859ffb0faec6354890bb10c5faa
Vilénio Kiala <andersonvitalena@hotmail.com>
2022-06-13 07:10:13 +0100

    committest

--- backend/src/services/UserServices/DeleteUserService.ts
+++ backend/src/services/UserServices/DeleteUserService.ts
@@ -4,6 +4,7 @@ export class DeleteUserService{
     constructor(private userRepository: IUserRepository){}
 
     async execute(id: string){
+        console.log("yea yea yea")
         const user = await this.userRepository.findOneById(id)
 
         await this.userRepository.delete(user)
